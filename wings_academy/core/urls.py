a41cda104b8450e9205489d446a39e5ac094b844
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-11-18 00:19:28 +0530

    added home page (demo) and created table for Blogs

---
+++ wings_academy/core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .views import home,all_blogs
+app_name="core"
+urlpatterns = [
+    path('',home,name="home"),
+    path('blogs/',all_blogs,name="all_blogs"),
+]
---
+++ wings_academy/core/views.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404,redirect
+from django.views.generic import ListView, DetailView, View
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from  django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+import json
+def home(request):
+    return render(request,"home.html")
+
+def all_blogs(request):
+    return HttpResponse("hello")
